f22c1a61682c96921597d121a84e7475b01dab3d
Pedro Gordo <pedro.gordo@P3UK-PGORDO-08.pcubed.com>
2016-02-22 16:23:34 +0000

    Adapter pattern classes

---
+++ AdapterPattern/src/mediaplayer/MediaAdapter.java
@@ -0,0 +1,30 @@
+package mediaplayer;
+
+import advancedmediaplayer.AdvancedMediaPlayer;
+import advancedmediaplayer.Mp4Player;
+import advancedmediaplayer.VlcPlayer;
+
+public class MediaAdapter implements MediaPlayer {
+
+	AdvancedMediaPlayer advancedMusicPlayer;
+
+	public MediaAdapter(String audioType) {
+
+		if (audioType.equalsIgnoreCase("vlc")) {
+			advancedMusicPlayer = new VlcPlayer();
+
+		} else if (audioType.equalsIgnoreCase("mp4")) {
+			advancedMusicPlayer = new Mp4Player();
+		}
+	}
+
+	@Override
+	public void play(String audioType, String fileName) {
+
+		if (audioType.equalsIgnoreCase("vlc")) {
+			advancedMusicPlayer.playVlc(fileName);
+		} else if (audioType.equalsIgnoreCase("mp4")) {
+			advancedMusicPlayer.playMp4(fileName);
+		}
+	}
+}
